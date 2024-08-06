33efa31815277be85ae8e965dd30e1f0e58eeeab
aacdnrz <aacdnrz@gmail.com>
2024-08-06 16:43:56 +0800

    WAAAAAAAAAAh

---
+++ script/script.js
@@ -0,0 +1,95 @@
+const footerElement = document.getElementById("footer-container");
+
+function renderFooter() {
+    footerElement.innerHTML = `
+        <h1>CONTACT US</h1>
+        
+            <div class="pup">
+                <i class="fab fa-facebook icon"><span class="text"> AAAAAAAAA</span></i>
+                <i class="fas fa-envelope icon"><span class="text"> AAAAAAAAA</span></i>
+                <i class="fas fa-phone icon"><span class="text"> AAAAAAAAA</span></i>  
+            </div>
+
+            <div class="container">
+
+            <div class="info-and-img">
+                <div class="container-img">
+                    <img src="../assets/about picture 2.jpg" alt="">
+                </div>  
+
+                <div class="info">
+                    <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
+                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
+                </div>
+            </div>
+
+            <div class="info-and-img">
+                <div class="container-img">
+                    <img src="../assets/about picture 2.jpg" alt="">
+                </div>  
+
+                <div class="info">
+                   <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
+                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
+                </div>
+            </div>
+
+            <div class="info-and-img">
+                <div class="container-img">
+                    <img src="../assets/about picture 2.jpg" alt="">
+                </div>  
+
+                <div class="info">
+                   <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
+                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
+                </div>
+            </div>
+
+            <div class="info-and-img">
+                <div class="container-img">
+                    <img src="../assets/about picture 2.jpg" alt="">
+                </div>  
+
+                <div class="info">
+                   <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
+                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
+                </div>
+            </div>
+
+            <div class="info-and-img">
+                <div class="container-img">
+                    <img src="../assets/about picture 2.jpg" alt="">
+                </div>  
+
+                <div class="info">
+                   <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
+                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
+                </div>
+            </div>
+
+            <div class="info-and-img">
+                <div class="container-img">
+                    <img src="../assets/about picture 2.jpg" alt="">
+                </div>  
+
+                <div class="info">
+                   <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
+                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
+                </div>
+            </div>
+
+            <div class="info-and-img">
+                <div class="container-img">
+                    <img src="../assets/about picture 2.jpg" alt="">
+                </div>  
+
+                <div class="info">
+                   <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
+                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+renderFooter();
