f95bf60bd57aa59012e5ef8994acd5e605e77125
aacdnrz <aacdnrz@gmail.com>
2024-08-07 08:13:28 +0800

    numbs

--- script/script.js
+++ script/script.js
@@ -5,9 +5,9 @@ function renderFooter() {
         <h1>CONTACT US</h1>
         
             <div class="pup">
-                <i class="fab fa-facebook icon"><span class="text"> AAAAAAAAA</span></i>
-                <i class="fas fa-envelope icon"><span class="text"> AAAAAAAAA</span></i>
-                <i class="fas fa-phone icon"><span class="text"> AAAAAAAAA</span></i>  
+                <i class="fab fa-facebook icon"><span class="text"> facebook.com/ThePUPOfficial</span></i>
+                <i class="fas fa-envelope icon"><span class="text"> inquire@pup.edu.ph</span></i>
+                <i class="fas fa-phone icon"><span class="text"> 0910 649 4049</span></i>  
             </div>
 
             <div class="container">
