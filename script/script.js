672ee103c02bd66774e690dc325f207175e58c77
alyson04 <alysoncalimag@gmail.com>
2024-08-07 14:04:22 +0800

    need pa ayusin

--- script/script.js
+++ script/script.js
@@ -1,3 +1,30 @@
+// THIS IS FOR ABOUT PAGE
+const Slides = document.querySelectorAll('.slide');
+                    let currentSlide = 0;
+            
+                    function showSlide(n) {
+                    Slides[currentSlide].classList.remove('active');
+                    currentSlide = (n + Slides.length) % Slides.length;
+                    Slides[currentSlide].classList.add('active');
+                    }
+            
+                    function nextSlide() {
+                    showSlide(currentSlide + 1);
+                    }
+            
+                    setInterval(nextSlide, 5000);
+
+//THIS IS FOR BLOG PAGE
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'flex';
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
+
+
+// THIS IS FOR FOOTER
 const footerElement = document.getElementById("footer-container");
 
 function renderFooter() {
@@ -18,3 +45,5 @@ function renderFooter() {
 }
 
 renderFooter();
+
+
