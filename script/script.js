f389c3e2b874fd82048bf09a1931694c6f743127
aacdnrz <aacdnrz@gmail.com>
2024-08-07 08:54:30 +0800

    changes

--- script/script.js
+++ script/script.js
@@ -2,94 +2,19 @@ const footerElement = document.getElementById("footer-container");
 
 function renderFooter() {
     footerElement.innerHTML = `
-        <h1>CONTACT US</h1>
-        
-            <div class="pup">
-                <i class="fab fa-facebook icon"><span class="text"> facebook.com/ThePUPOfficial</span></i>
-                <i class="fas fa-envelope icon"><span class="text"> inquire@pup.edu.ph</span></i>
-                <i class="fas fa-phone icon"><span class="text"> 0910 649 4049</span></i>  
-            </div>
+    <h2>CONTACT US</h2>
+    
+    <div class="pup">
+        <i class="fab fa-facebook icon"><span class="text"> facebook.com/ThePUPOfficial</span></i>
+        <i class="fas fa-envelope icon"><span class="text"> inquire@pup.edu.ph</span></i>
+        <i class="fas fa-phone icon"><span class="text"> 0910 649 4049</span></i>  
+    </div>
 
-            <div class="container">
+    <div class="container">
+        <p>&copy; Polytechnic University of the Philippines. All rights reserved.</p>
+    </div>
+`;
 
-            <div class="info-and-img">
-                <div class="container-img">
-                    <img src="../assets/about picture 2.jpg" alt="">
-                </div>  
-
-                <div class="info">
-                    <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
-                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
-                </div>
-            </div>
-
-            <div class="info-and-img">
-                <div class="container-img">
-                    <img src="../assets/about picture 2.jpg" alt="">
-                </div>  
-
-                <div class="info">
-                   <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
-                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
-                </div>
-            </div>
-
-            <div class="info-and-img">
-                <div class="container-img">
-                    <img src="../assets/about picture 2.jpg" alt="">
-                </div>  
-
-                <div class="info">
-                   <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
-                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
-                </div>
-            </div>
-
-            <div class="info-and-img">
-                <div class="container-img">
-                    <img src="../assets/about picture 2.jpg" alt="">
-                </div>  
-
-                <div class="info">
-                   <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
-                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
-                </div>
-            </div>
-
-            <div class="info-and-img">
-                <div class="container-img">
-                    <img src="../assets/about picture 2.jpg" alt="">
-                </div>  
-
-                <div class="info">
-                   <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
-                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
-                </div>
-            </div>
-
-            <div class="info-and-img">
-                <div class="container-img">
-                    <img src="../assets/about picture 2.jpg" alt="">
-                </div>  
-
-                <div class="info">
-                   <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
-                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
-                </div>
-            </div>
-
-            <div class="info-and-img">
-                <div class="container-img">
-                    <img src="../assets/about picture 2.jpg" alt="">
-                </div>  
-
-                <div class="info">
-                   <br> <i class="fas fa-envelope icon"></i> <span class="txt">AAAAAAAAA</span> </br>
-                    <i class="fas fa-phone icon"></i> <span class="txt">AAAAAAAAA</span>   
-                </div>
-            </div>
-        </div>
-    `;
 }
 
 renderFooter();
