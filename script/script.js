cff3e575c47672e51e5b53589ba9b23666b60e6e
aacdnrz <aacdnrz@gmail.com>
2024-08-08 14:20:20 +0800

    fixed

--- script/script.js
+++ script/script.js
@@ -30,12 +30,14 @@ const footerElement = document.getElementById("footer-container");
 function renderFooter() {
     footerElement.innerHTML = `
 
-    <div class="container-footer">
+    <div class="container">
         <p>&copy; Polytechnic University of the Philippines. All rights reserved.</p>
     </div>
 `;
 
 }
+
+renderFooter();
 
 renderFooter();
 
